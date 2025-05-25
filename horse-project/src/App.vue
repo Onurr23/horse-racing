382585d7c71f2524ba2291fe00a0a14cae16143a
Onur Kara <60196841+Onurr23@users.noreply.github.com>
2025-05-25 13:54:13 +0300

    main structure in app is created

--- horse-project/src/App.vue
+++ horse-project/src/App.vue
@@ -1,11 +1,45 @@
 <script setup>
- 
+import ControlPanel from './components/ControlPanel.vue';
+import HorseList from './components/HorseList.vue';
+import RaceTrack from './components/RaceTrack.vue';
+import { useStore } from 'vuex';
+import { onMounted } from 'vue'
+
+const store = useStore();
+
+onMounted(() => {
+    store.dispatch('generateHorses');
+})
+
 </script>
 
 <template>
 
+    <ControlPanel />
+    <div class="race-container">
+        <div class="race-element">
+            <HorseList />
+        </div>
+        <div class="race-element">
+            <RaceTrack />
+        </div>
+        <div class="race-element">
+            <div></div>
+        </div>
+    </div>
+
+
 </template>
 
 <style scoped>
+.race-container {
+    display: flex;
+}
 
+.race-element {
+    flex: 1;
+    background: #eee;
+    padding: 20px;
+    text-align: center;
+}
 </style>
