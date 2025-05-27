371c2cab4dc508669229304768134e86e8053356
Onur Kara <60196841+Onurr23@users.noreply.github.com>
2025-05-27 12:12:51 +0300

    noise is increased to decrease effect of condition in race

--- horse-project/src/utils/utils.js
+++ horse-project/src/utils/utils.js
@@ -1,7 +1,7 @@
 export function calculateSpeedPerTick(condition, distance) {
     const baseSpeed = 8.57
     const conditionFactor = 1 + (condition / 100) 
-    const randomNoise = (Math.random() - 0.5) * 0.2 
+    const randomNoise = (Math.random() - 0.5) * 0.5
     const distanceFactor = 1200 / distance 
   
     const finalSpeed = baseSpeed * (conditionFactor + randomNoise) * distanceFactor
